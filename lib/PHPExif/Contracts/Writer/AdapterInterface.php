36fb078c117333087346394aa8d4eaa090a4990a
Sanchit Singh <sanckde@gmail.com>
2025-01-30 22:24:34 +0100

    Creating AdapterInterface Writer Contract

---
+++ lib/PHPExif/Contracts/Writer/AdapterInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @codeCoverageIgnore
+ */
+
+namespace PHPExif\Contracts\Writer;
+
+use PHPExif\Exif;
+use PHPExif\Writer\PhpExifWriterException;
+
+interface AdapterInterface
+{
+    /**
+     * Writes the EXIF data to the given file
+     *
+     * @param Exif $exif
+     * @param string $file
+     * @throws PhpExifWriterException If the EXIF data could not be written
+     */
+    public function writeExifToFile(Exif $exif, string $file): void;
+}
