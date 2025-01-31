2c67901b148b2b67421e2fac63d082b1a02b00a0
Sanchit Singh <sanckde@gmail.com>
2025-02-01 00:03:43 +0100

    Initial work on AbstractAdapter for Writer

---
+++ lib/PHPExif/Adapter/Writer/AbstractAdapter.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace PHPExif\Adapter\Writer;
+
+use PHPExif\Adapter\ConvertToUTF8Trait;
+use PHPExif\Contracts\Writer\AdapterInterface;
+use PHPExif\Contracts\Writer\ExtractorInterface;
+use PHPExit\Contracts\MapperInterface;
