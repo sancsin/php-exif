4fcada894d8fefdeb246116cabdbbaa1b15b0654
Sanchit Singh <sanchitsingh88@gmail.com>
2025-03-02 23:27:23 +0100

    Added Accessor class tests

---
+++ tests/PHPExif/Extractor/AccessorTest.php
@@ -0,0 +1,131 @@
+<?php
+
+use PHPExif\Extractor\Accessor;
+
+class AccessorTest extends \PHPUnit\Framework\TestCase
+{
+    protected function setUp(): void {}
+
+    public function testAccessorCallsDetermineAccessor()
+    {
+        $input =  array(
+            'foo' => 'bar',
+        );
+
+        /** @var Accessor|\PHPUnit\Framework\MockObject\MockObject $mock */
+        $mock = $this->getMockBuilder(Accessor::class)
+            ->onlyMethods(array('determineAccessor'))
+            ->getMock();
+
+        $mock->expects($this->exactly(count($input)))
+            ->method('determineAccessor')
+            ->will($this->returnValue('getFoo'));
+
+        $object = new AccessorTestClass();
+
+        $mock->extract($object, $input);
+    }
+
+    public function testExtractCallsAccessorOnObject()
+    {
+        $inputs = array(
+            'faz' => 'foo',
+            'baz' => 'bar'
+        );
+
+        $expected = array(
+            'faz' => 'faz value',
+            'baz' => 'baz value'
+        );
+
+        $mock = $this->getMockBuilder(AccessorTestClass::class)
+            ->onlyMethods(array('getFoo', 'getBar'))
+            ->getMock();
+
+        $mock->expects($this->once())
+            ->method('getFoo')
+            ->willReturn($expected['faz']);
+
+        $mock->expects($this->once())
+            ->method('getBar')
+            ->willReturn($expected['baz']);
+
+        $accessor = new Accessor();
+        $accessor->extract($mock, $inputs);
+    }
+
+    public function testExtractReturnArray()
+    {
+        $inputs = array(
+            'faz' => 'foo',
+            'baz' => 'bar'
+        );
+
+        $expected = array(
+            'faz' => 'faz value',
+            'baz' => 'baz value'
+        );
+
+        $mock = $this->getMockBuilder(AccessorTestClass::class)
+            ->onlyMethods(array('getFoo', 'getBar'))
+            ->getMock();
+
+        $mock->method('getFoo')->willReturn($expected['faz']);
+        $mock->method('getBar')->willReturn($expected['baz']);
+
+        $accessor = new Accessor();
+        $result = $accessor->extract($mock, $inputs);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testExtractReturnArrayWithIgnoredValues()
+    {
+        $inputs = array(
+            'faz' => 'foo',
+            'baz' => 'bar'
+        );
+
+        $expected = array(
+            'faz' => 'faz value'
+        );
+
+        $mock = $this->getMockBuilder(AccessorTestClass::class)
+            ->onlyMethods(array('getFoo', 'getBar'))
+            ->getMock();
+
+
+        // Test with null value
+
+        $mock->method('getFoo')->willReturn($expected['faz']);
+        $mock->method('getBar')->willReturn(null);
+
+        $accessor = new Accessor();
+        $result = $accessor->extract($mock, $inputs);
+
+        $this->assertEquals($expected, $result);
+
+        // Test with empty string
+
+        $mock->method('getBar')->willReturn('');
+
+        $result = $accessor->extract($mock, $inputs);
+
+        $this->assertEquals($expected, $result);
+
+        // Test with false
+
+        $mock->method('getBar')->willReturn(false);
+
+        $result = $accessor->extract($mock, $inputs);
+
+        $this->assertEquals($expected, $result);
+    }
+}
+
+class AccessorTestClass
+{
+    public function getFoo() {}
+
+    public function getBar() {}
+}
