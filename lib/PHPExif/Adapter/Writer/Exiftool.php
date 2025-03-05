db070ee420e238c4bdabd43c0c93f6bb0e76e065
Sanchit Singh <sanchitsingh88@gmail.com>
2025-03-05 22:35:14 +0100

    Exiftool Writer Adapter

--- lib/PHPExif/Adapter/Writer/Exiftool.php
+++ lib/PHPExif/Adapter/Writer/Exiftool.php
@@ -37,5 +37,32 @@ class Exiftool extends AbstractAdapter
         $data = $extractor->extract($exif, $mapper->getMap());
 
         $rawData = $mapper->mapRawData($data);
+
+        $command = $this->getExifWriteCommand($rawData, $file, $encoding);
+
+        $result = $this->getCliOutput($command);
+    }
+
+    private function getExifWriteCommand(array $rawData, string $file, string $encoding): string
+    {
+        $command = [];
+        $command[] = $this->toolPath;
+        $command[] = ' ';
+        foreach ($rawData as $key => $value) {
+            $command[] = "-if";
+            $command[] = " ";
+            $command[] = '"$' . $key . '"';
+            $command[] = " ";
+            $command[] = "-$key=" . '"' . $value . '"';
+            $command[] = " ";
+        }
+
+        $command[] = "-overwrite_original";
+        $command[] = " ";
+        $command[] = $encoding;
+        $command[] = " ";
+        $command[] = escapeshellarg($file);
+
+        return implode('', $command);
     }
 }
