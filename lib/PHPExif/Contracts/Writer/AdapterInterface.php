7af1668c6290a193283cfd01e76e79b516a7c038
Sanchit Singh <sanchitsingh88@gmail.com>
2025-04-04 00:06:53 +0200

    Added doc for Writer AdapterrInterface

--- lib/PHPExif/Contracts/Writer/AdapterInterface.php
+++ lib/PHPExif/Contracts/Writer/AdapterInterface.php
@@ -9,14 +9,22 @@ namespace PHPExif\Contracts\Writer;
 use PHPExif\Exif;
 use PHPExif\Writer\PhpExifWriterException;
 
+/**
+ * PHP Exif Writer Adapter
+ *
+ * Defines the interface for writer adapters
+ * @category   PHPExif
+ * @package    Writer
+ */
 interface AdapterInterface
 {
     /**
      * Writes the EXIF data to the given file
      *
-     * @param Exif $exif
-     * @param string $file
+     * @param Exif $exif The EXIF data to be written
+     * @param string $file The file path to write the EXIF data to
+     * @return string The output of the command
      * @throws PhpExifWriterException If the EXIF data could not be written
      */
-    public function writeExifToFile(Exif $exif, string $file): string|false;
+    public function writeExifToFile(Exif $exif, string $file): string;
 }
